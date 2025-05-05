5eba94390440dece3fcedaadbfd55e2674129837
Rodrigo Caixeta <rodrigo@projeta.digital>
2025-05-05 16:44:37 -0300

    fullscreen

--- android/app/src/main/java/com/rodrigocaixeta/quizapp/MainActivity.java
+++ android/app/src/main/java/com/rodrigocaixeta/quizapp/MainActivity.java
@@ -1,5 +1,22 @@
 package com.rodrigocaixeta.quizapp;
 
+import android.os.Bundle;
+import android.view.View;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    // Coloca o app em modo imersivo total (esconde status e navbar)
+    getWindow().getDecorView().setSystemUiVisibility(
+      View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+      | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+      | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+      | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+      | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+      | View.SYSTEM_UI_FLAG_FULLSCREEN
+    );
+  }
+}
